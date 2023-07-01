238401f9e86a7e9e575eafa816c1c599ee3cc9d4
ramkumar <tempdevs103@example.com>
2023-07-01 10:28:31 +0530

    Refactored.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
-dotenv.config({path:"../.env"});
+dotenv.config();
 
 const PORT = process.env.BACKEND_PORT || 3001
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 
 app.listen(PORT,()=>{
