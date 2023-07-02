a1d7e5b5316bcde92dfa0311d936a09675e08543
ramkumar <tempdevs103@example.com>
2023-07-02 14:43:29 +0530

    Added new case.

--- client/src/context/StateReducers.js
+++ client/src/context/StateReducers.js
@@ -4,6 +4,7 @@ export const initialState = {
   userInfo: undefined,
   newUser: false,
   contactpage: false,
+  currentChatUser: undefined,
 };
 
 const reducer = (state, action) => {
@@ -22,6 +23,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         contactpage: !state.contactpage,
+      };
+    case reducerCases.SET_CURRENT_CHAT_USER:
+      return {
+        ...state,
+        currentChatUser: action.user,
       };
     default:
       return state;
