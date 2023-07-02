8b23bcd95bd70fafa17b166fd02cdef3f8baf411
ramkumar <tempdevs103@example.com>
2023-07-02 14:42:54 +0530

    Implemented ContactsList component.

--- client/src/components/Chatlist/ChatLIstItem.jsx
+++ client/src/components/Chatlist/ChatLIstItem.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Avatar from "../common/Avatar";
+import { useStateProvider } from "@/context/StateContext";
 
-function ChatLIstItem() {
-  return <div>ChatLIstItem</div>;
+function ChatListItem({ data, isContactPage }) {
+  const [{ userInfo }] = useStateProvider();
+
+  return (
+    <div className="flex items-center cursor-pointer hover:bg-background-default-hover">
+      <div className="min-w-fit px-5 pt-3 pb-3">
+        <Avatar
+          size="sm"
+          image={data?.profilePicture || "/default_avatar.png"}
+        />
+      </div>
+      <div className="min-h-full flex flex-col justify-center mt-3 pr-2 w-full">
+        <div className="flex justify-between">
+          <div>
+            <span className="text-white">
+              {userInfo?.name === data?.name ? "YOU" : data?.name}
+            </span>
+          </div>
+        </div>
+        <div className="flex border-b border-conversation-border pb-2 pt-2 pr-2">
+          <div className="flex justify-between w-full">
+            <span className="text-secondary line-clamp-1 text-sm">{data?.about || "\u00A0"}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ChatLIstItem;
+export default ChatListItem;
--- client/src/components/Chatlist/ContactsList.jsx
+++ client/src/components/Chatlist/ContactsList.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { GET_ALL_CONTACTS_ROUTE } from "@/utils/ApiRoutes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BiArrowBack, BiSearchAlt2 } from "react-icons/bi";
+import ChatListItem from "./ChatListItem";
 
 function ContactsList() {
-  return <div>ContactsList</div>;
+  const [allContacts, setAllContacts] = useState([]);
+  const [{}, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const getAllContacts = async () => {
+      try {
+        const {
+          data: { users },
+        } = await axios.get(GET_ALL_CONTACTS_ROUTE);
+        setAllContacts(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllContacts();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="h-20 flex  items-end px-3 py-4">
+        <div className="flex items-center gap-12 text-white">
+          <BiArrowBack
+            className="cursor-pointer text-xl"
+            onClick={() => dispatch({ type: reducerCases.SET_CONTACT_PAGE })}
+          />
+          <span>New Chat</span>
+        </div>
+        
+      </div>
+      <div className="bg-search-input-container-background  py-3 px-5 items-center gap-3 h-full overflow-auto custom-scrollBar">
+          <div className="bg-panel-header-background flex items-center gap-5 px-3 py-1 rounded-lg flex-grow">
+            <div>
+              <BiSearchAlt2 className="text-panel-header-icon cursor-pointer text-xl" />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Search or start new chat"
+                className="text-sm bg-transparent focus:outline-none text-white w-full"
+              />
+            </div>
+          </div>
+          {Object.entries(allContacts).map(([initialLetter,userList])=>{
+            return (
+              <div key={initialLetter}>
+                <div className="text-teal-light  pl-8 py-5">{initialLetter}</div>
+                {userList.map((contact)=>{
+                  return <ChatListItem
+                  data={contact}
+                  isContactPage={true}
+                  key={contact.id}
+                  />
+                })}
+              </div>
+            )
+          })}
+        </div>
+    </div>
+  );
 }
 
 export default ContactsList;
