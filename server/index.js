9da47e738ab06a9077a4777373e9252eb9dca3da
ramkumar <tempdevs103@example.com>
2023-07-11 16:37:02 +0530

    Added orgin

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://weasel-psi.vercel.app",
   },
 });
 
